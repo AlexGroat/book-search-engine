2abdf68d8002b8a7148b5b90ebb638843d94f382
Alex Groat <alex.groat.95@gmail.com>
2022-02-03 16:57:20 +1030

    actually adding the middleware helps im such a

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,19 @@ const { signToken } = require('../utils/auth');
 
 const resolver = {
     Query: {
-        
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id })
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+    },
+
+    Mutation:{
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user};
+        }
     }
 }
