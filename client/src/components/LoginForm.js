925953473b5d4701141d4ecf2fefa884117bd497
Alex Groat <alex.groat.95@gmail.com>
2022-02-06 23:16:39 +1030

    up to save book

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -41,7 +41,6 @@ const LoginForm = () => {
       Auth.login(data.login.token);
     } catch (err) {
       console.error(error);
-      setShowAlert(true);
     }
 
     setUserFormData({
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
   const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
