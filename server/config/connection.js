1a750b440e84b91c253cb7e0cdcae718eb8a1af7
Alex Groat <alex.groat.95@gmail.com>
2022-02-08 14:44:33 +1030

    update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-console.log('mongoose start')
+console.log(process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
